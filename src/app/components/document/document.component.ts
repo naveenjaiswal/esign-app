e539c0226aeb0b43ab29879e51d8d5f23709ac11
Naveen Jaiswal <nav2jaiswal@gmail.com>
2021-07-14 20:26:47 +0530

    Code updated

--- src/app/components/document/document.component.ts
+++ src/app/components/document/document.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-document',
@@ -8,30 +8,31 @@ import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./document.component.scss']
 })
 export class DocumentComponent implements OnInit {
- status:string="hello";
- label:string="hello";
- closeModal: string;
- dataForm: FormGroup;
- 
- constructor(private modalService: NgbModal,private formBuilder:FormBuilder) {}
+  status: string = "Register Adhaar";
+  label: string = "Mutual Non-Disclosure Agreement";
+  closeModal: string;
+  dataForm: FormGroup;
+
+  constructor(private modalService: NgbModal, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.dataForm = this.formBuilder.group({
-      adhaar: ['', [Validators.required, Validators.email]],
-      otp: ['', [Validators.required, Validators.email]],
+      adhaar: ['', [Validators.required, Validators.pattern('^[0-9]{12}$')]],
+      otp: ['', [Validators.required, Validators.pattern('^[0-9]{6}$')]],
+      kyc: [false, Validators.requiredTrue]
     });
   }
 
-  triggerModal(content:any) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((res) => {
+  triggerModal(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((res) => {
       this.closeModal = `Closed with: ${res}`;
     }, (res) => {
       this.closeModal = `Dismissed`;
     });
   }
-  
+
   isAdhaarValid(): boolean {
-    return this.dataForm.valid;
+    return this.dataForm.controls.adhaar.valid
   }
 
   isOtpValid(): boolean {
@@ -39,7 +40,16 @@ export class DocumentComponent implements OnInit {
   }
 
   get f() {
+    console.log(this.dataForm.controls)
     return this.dataForm.controls;
   }
 
+  onVerify() { }
+
+  onSubmit() {
+    this.status = "Signing...";
+    setTimeout(() => {
+      this.status = "Signed";
+    }, 3000)
+  }
 }
